a15cf3854d4a0c02bf34ec8eec7ca57958a3de7c
Андрій Шайнога <andrii.shainoha.pz.2018@lpnu.ua>
2020-03-29 23:13:27 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class StudentEditor
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudentEditor));
+            this.adfdafToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fdsdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip5 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip6 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // adfdafToolStripMenuItem
+            // 
+            this.adfdafToolStripMenuItem.Name = "adfdafToolStripMenuItem";
+            this.adfdafToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.adfdafToolStripMenuItem.Text = "adfdaf";
+            // 
+            // fdsdfToolStripMenuItem
+            // 
+            this.fdsdfToolStripMenuItem.Name = "fdsdfToolStripMenuItem";
+            this.fdsdfToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.fdsdfToolStripMenuItem.Text = "fdsdf";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1007, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(30, 128);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(798, 367);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Surname";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 93;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Day";
+            this.columnHeader3.Width = 94;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Month";
+            this.columnHeader4.Width = 94;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Year";
+            this.columnHeader5.Width = 69;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Programming";
+            this.columnHeader6.Width = 97;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Mathematic";
+            this.columnHeader7.Width = 96;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Physics";
+            this.columnHeader8.Width = 83;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Write in file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add from File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(834, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 33);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sort and Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(175, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Write in ListView";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(563, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 32);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(847, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(844, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Mark for deleting";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // StudentEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1007, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "StudentEditor";
+            this.Text = "StudentEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem adfdafToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fdsdfToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.ToolTip toolTip5;
+        private System.Windows.Forms.ToolTip toolTip6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class StudentEditor : Form
+    {
+        List<Student> obj;
+        public StudentEditor()
+        {
+            InitializeComponent();
+            //imageList1.Images.Add(Image.FromFile(@"C:\Users\andri\Desktop\shutterstock_160918730.jpg"));
+            obj = new List<Student>();
+            toolTip1.SetToolTip(this.button1, "Read and add information from file to List");
+            toolTip2.SetToolTip(this.button4, "Write information from file to ListView");
+            toolTip3.SetToolTip(this.button3, "Sorting and Deleting by task");
+            toolTip4.SetToolTip(this.button2, "Write information to a file");
+            toolTip5.SetToolTip(this.button5, "Clear items from ListView");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //з файлами всьо работає
+        {   
+        }
+        private void fromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string writePath = @"C:\Users\andri\Desktop\ПЗ\ВІПЗ\Лаби ВІПЗ\4 Лаба програми\textout.txt";
+            using (StreamWriter sw = new StreamWriter(writePath))
+            {
+                foreach (var d in obj)
+                {
+                    sw.Write(d.Surname+" ");
+                    sw.Write(d.Name + " ");
+                    sw.Write(d.Date[0] + " ");
+                    sw.Write(d.Date[1] + " ");
+                    sw.Write(d.Date[2] + " ");
+                    sw.Write(d.Marks[0] + " ");
+                    sw.Write(d.Marks[1] + " ");
+                    sw.Write(d.Marks[2] + " ");
+                    sw.WriteLine();
+                }
+                sw.Close();
+            }
+
+        }
+
+        private void sortAndDeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string readPath = @"C:\Users\andri\Desktop\ПЗ\ВІПЗ\Лаби ВІПЗ\4 Лаба програми\textin.txt";
+
+            using (var sr = new StreamReader(readPath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var input = sr.ReadLine();
+                    var regex = new Regex(@"^([a-zA-Z]+) ([a-zA-Z]+) (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)$");
+                    if (regex.IsMatch(input))
+                    {
+                        var match = regex.Match(input);
+                        var groups = match.Groups;
+
+                        var surname = groups[1].Value;
+                        var firstname = groups[2].Value;
+
+                        var date =
+                            groups.Cast<Group>().Skip(3)
+                            .Take(3)
+                            .Select(g => int.Parse(g.Value))
+                            .ToArray();
+
+                        var marks =
+                            groups.Cast<Group>().Skip(6)
+                            .Take(3)
+                            .Select(g => int.Parse(g.Value))
+                            .ToArray();
+
+                       var student = new Student(surname, firstname, date, marks);
+                        obj.Add(student);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error with reading");                  //переписати  під норм вивід
+                    } 
+
+                }
+                sr.Close();
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            obj.Sort();
+
+            obj.RemoveAll(x =>
+            {
+                for (int i = 0; i < 3; ++i) if (x.Marks[i] == n) return true;
+                return false;
+            });
+            listView1.Items.Clear();
+            foreach (var students in obj)
+            {
+                var listvi = new ListViewItem(students.Surname);
+                listvi.SubItems.Add(students.Name);
+                listvi.SubItems.Add(students.Date[0].ToString());
+                listvi.SubItems.Add(students.Date[1].ToString());
+                listvi.SubItems.Add(students.Date[2].ToString());
+                listvi.SubItems.Add(students.Marks[0].ToString());
+                listvi.SubItems.Add(students.Marks[1].ToString());
+                listvi.SubItems.Add(students.Marks[2].ToString());
+                listView1.Items.Add(listvi);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (var students in obj)
+            {
+                var listvi = new ListViewItem(students.Surname);
+                listvi.SubItems.Add(students.Name);
+                listvi.SubItems.Add(students.Date[0].ToString());
+                listvi.SubItems.Add(students.Date[1].ToString());
+                listvi.SubItems.Add(students.Date[2].ToString());
+                listvi.SubItems.Add(students.Marks[0].ToString());
+                listvi.SubItems.Add(students.Marks[1].ToString());
+                listvi.SubItems.Add(students.Marks[2].ToString());
+                listView1.Items.Add(listvi);
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+    public class Student : IComparable<Student>
+    {
+        public string Surname;
+        public string Name;
+        public int[] Date = new int[3];
+        public int[] Marks = new int[3];
+        public Student(string s,string n,int[]d,int []m)
+        {
+            Surname = s;
+            Name = n;
+            Date = d;
+            Marks = m;
+        }
+
+        public int CompareTo(Student other)
+        {
+            if (this.Date[2] > other.Date[2])
+            {
+                return 1;
+            }
+            else if (this.Date[2] == other.Date[2])
+            {
+                if (this.Date[1] > other.Date[1])
+                {
+                    return 1;
+                }
+                else if (this.Date[1] == other.Date[1])
+                {
+                    if (this.Date[0] > other.Date[0])
+                    {
+                        return 1;
+                    }
+                    else if(this.Date[0] == other.Date[0])
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+}
